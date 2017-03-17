16f295ad69f139d721d0e3851db14587dfc2c417
vochavillo <vochavillo@gmail.com>
2017-03-16 20:11:04 -0700

    Added script file that prints story

---
+++ story.py
@@ -0,0 +1,9 @@
+from fire_foo import current_week_calls, last_week_calls
+
+current_week_count =len(current_week_calls)
+last_week_count = len(last_week_calls)
+
+message =
+"""
+This week there were {current} 911 calls made to the Seattle Fire Department. Last week {previous} calls were made to the department.
+"""
